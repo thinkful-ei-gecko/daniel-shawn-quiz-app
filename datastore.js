ad0dea6292eaa65045658cbff41f2da3a5079227
Daniel <herrnichols@gmail.com>
2019-08-16 17:46:44 -0400

    quiz answers added to Datastore.js

--- datastore.js
+++ datastore.js
@@ -2,6 +2,113 @@
 
 /*
 
+// These are the questions and answers for the Quiz
+const DATASTORE = [
+    {
+      question: 'The "spiritual", if not the official, midpint of the Appalachian Trail is located where?',
+      answers: [
+        'Waynesboro, VA',
+        'Gatliinburg, TN',
+        "Harper's Ferry, WV",
+        "Pine Furnace State Park, PA"
+      ],
+    correctAnswer: "Harper's Ferry, WV",
+    icon: icon,
+    alt: alt
+  },
+  {
+    question: 'The northbound endpoint of the trail is located in which state park?',
+    answers: [
+      'Acadia National Park',
+      'Baxter State Park',
+      'Wolfe Neck Woods State Park',
+      'Two Lights State Park'
+    ],
+    correctAnswer: 'Baxter State Park'
+  },
+  {
+    question: 'What is the slang term for taking a "shortcut" down a side trail?',
+    answers: [
+      'shortcutting',
+      'Running',
+      'Blue blazing',
+      'Slackpacking'
+    ],
+    correctAnswer: 'Blue blazing'
+  },
+  {
+    question: 'The home of the celebrated "Trail Days" festival',
+    answers: [
+      'Damascus, VA',
+      'Hot Springs, NC',
+      'Andover, ME',
+      'Hiawassee, GA'
+    ],
+    correctAnswer: 'Damascus, VA'
+  },
+  {
+    question: 'Who completed the first ever solo "thru-hike" of the Appalachian Trail?',
+    answers: [
+      '"Grandma" Gatewood',
+      '"Baltimore" Jack',
+      '"Two Sticks"',
+      'Earl Schaffer'
+    ],
+    correctAnswer: 'Earl Schaffer'
+  },
+  {
+    question: 'The first, and for many the last, outfitter encountered on the AT is located at...',
+    answers: [
+      'Blood Mountain',
+      'Newfound Gap',
+      'Neels Gap',
+      'Grafton Notch'
+    ],
+    correctAnswer: 'Neels Gap'
+  },
+  {
+    question: 'The Appalachian Trail does not cross over which of the following peaks?',
+    answers: [
+      'Grandfather Mountain',
+      'Wildcat Mountain',
+      '"The Priest"',
+      'Saddleback Mountain'
+    ],
+    correctAnswer: 'Grandfather Mountain'
+  },
+  {
+    question: 'Kind souls who spend the hiking season providing services such as shuttles, food, and shelter to weary hikers are known affectionately as:',
+    answers: [
+      'Good Samaritans',
+      'Trail Angels',
+      'Caretakers',
+      'Ridge Runners'
+    ],
+    correctAnswer: 'Trail Angels'
+  },
+  {
+    question: 'Catching some kind of break on the trail, be it a free meal, a free stay, or in some cases an unexpected bottle of water is known as...',
+    answers: [
+      'Good luck',
+      'Field Fortune',
+      'Camp Karma',
+      'Trail Magic'
+    ],
+    correctAnswer: 'Trail Magic'
+  },
+  {
+    question: 'The Appalachian Trail merges and runs concurrently with another major long-distance trail. What is the name of that trail?',
+    answers: [
+      'Mountains-to-Sea Trail',
+      'Art Loeb Trail',
+      'The Makaye Memorial Path',
+      'The Long Trail'
+    ],
+    correctAnswer: 'The Long Trail'
+  },
+]
+
+
 const datastore = [
   {
     question: 'question1',
