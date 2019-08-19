39b3085fd12ea4e2ee6f2fb2828962beaa781235
shawn-collette <shawn.collette@gmail.com>
2019-08-19 09:11:53 -0600

    first commit of branch

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
+/* eslint-disable no-console */
 /* eslint-disable quotes */
 /* eslint-disable no-undef */
 /* eslint-disable strict */
-
-
-//function: start quiz button
-
-//variable: question counter
-//variable: score
-
-//function: generate question from counter, return counter++
-
 
 const DATASTORE = [
   {
@@ -52,27 +44,20 @@ const DATASTORE = [
     ],
     correctAnswer: 'D',
   }
-
   //more questions
-
 ];
 
+let questionNumber;
+let score;
 
-
-
-let questionNumber = 0;
-let score = 0;
-
-
-function quizQuestion() {
-  //if statement //put classes?
-  $('body').append(`<div class="question-${questionNumber}">
-  <form class="answer-form">
+function writeQuestion(questionNumber) {
+  $('body').append(`<div id="question-asker" class="question-${questionNumber} hide">
+  <form class="answer-form play-box">
     <fieldset>
 
       <legend>${DATASTORE[questionNumber].question}</legend>
 
-      <label class="answerOption1">
+      <label class="answerOption">
         <input type="radio" value="${DATASTORE[questionNumber].answers[0]}" name="answer" required>
           <span>${DATASTORE[questionNumber].answers[0]}</span>
   </label>
@@ -99,58 +84,132 @@ function quizQuestion() {
   </div>`);
 }
 
-function questionCount() {
+function questionCounter() {
   questionNumber++;
+  updateScoreboard();
 }
 
+function updateScoreboard(value) {
+  if (value === 'correct') {
+    score++;
+  }
+  $('.score').text(score);
+  $('.questionNumber').text(questionNumber);
+}
 
+function checkAnswer(userAnswer) {
+  let correctIncorrect;
+  if (userAnswer === DATASTORE[questionNumber].correctAnswer) {
+    correctIncorrect = 'correct';
+  }
+  else {
+    correctIncorrect = 'incorrect';
+  }
+  updateScoreboard(correctIncorrect);
+  cleanWindow();
+  $('#correct-incorrect').append(`${userAnswer} is ${correctIncorrect.toUpperCase()}`);
+  $('#answer-text').append(`
+    <p>Q: ${DATASTORE[questionNumber].question}</p>
+    <p>A: ${DATASTORE[questionNumber].correctAnswer}</p>
+    `);
+  $('#show-answer').fadeToggle('slow');
+}
 
-function submitHandle() {
+function submitButton() {
   $('.answer-form').on('submit', event => {
     event.preventDefault();
-    let userAnswer = $('input:checked');
-    console.log(userAnswer);
-    questionCount();
-    quizQuestion(questionNumber);
+    $('#question-asker').fadeToggle('slow', function () {
+      let userAnswer = $('input:checked').val();
+      checkAnswer(userAnswer);
+      questionCounter();
+      nextButton();
+    });
   });
 }
 
-
-
-
-
-// function questionGenerator() {
-//   $('.quiz-display').append(`
-//     <h1>this is an appended question</h1>
-//     $(DATASTORE[questionNumber].question}</p>
-//     `);
-// }
-
-
+function nextButton() {
+  $('#next-question').on('click', function () {
+    if (DATASTORE.length === questionNumber) {
+      $('#show-answer').fadeOut('slow', function () {
+        cleanWindow();
+        scoreScreen();
+      });
+    }
+    else {
+      $('#show-answer').fadeOut('slow', function () {
+        cleanWindow();
+        writeQuestion(questionNumber);
+        $('#question-asker').fadeToggle('slow');
+        submitButton();
+      });
+    }
+  });
+}
 
 function startQuiz() {
-  $('.quiz-display').on('click', function (event) {
-    $('.quiz-display').remove();
-    quizQuestion();
-    submitHandle();
-    //$('.quiz-display').append(quizQuestion);
-    //$('.questionAnswerForm').css('display', 'block');
-    //$('.questionNumber').text(1);
+  $('#quiz-start').on('click', function () {
+    $('.start-display').fadeOut('slow', function () {
+      writeQuestion(questionNumber);
+      $('#question-asker').fadeIn('slow');
+      submitButton();
+    });
+  });
+}
+
+function cleanWindow() {
+  $('#show-answer').hide();
+  $('#correct-incorrect').empty();
+  $('#answer-text').empty();
+  $('#question-asker').remove();
+}
+
+// function scoreScreen() {   //rewrite
+//   cleanWindow();
+//   resetButton();
+//   $('#correct-incorrect').append((score / DATASTORE.length) * 100 + '%');
+//   $('#answer-text').append(`
+//     <h1>You answered ${score} of ${DATASTORE.length} question correctly.</h1>
+//     <button id="reset-button">Try again?</button>
+//     `);
+//   $('#next-question').hide();
+//   $('#show-answer').fadeIn('slow');
+// }
+
+function scoreScreen() {
+  cleanWindow();
+  let scorePercent = (score / DATASTORE.length) * 100;
+  $('#score-text').append(`
+    <h1>${scorePercent}%</h1>
+    <h1>You answered ${score} of ${DATASTORE.length} question correctly.</h1>
+    `);
+  $('#score-screen').fadeIn('slow');
+}
+
+function init() {
+  questionNumber = 0;
+  score = 0;
+  $('.totalQuestions').text(DATASTORE.length);
+  updateScoreboard();
+}
+
+function resetButton() {
+  $('#reset-button').on('click', function () {
+    window.location.reload(false);
+  });
+}
+
+function resetIcon() {
+  $('#reset-mountain').on('click', function () {
+    window.location.reload(false);
   });
 }
 
 
 function main() {
-  //init();  //click handlers
+  init();
   startQuiz();
-
+  resetIcon();
+  resetButton();
 }
 
 $(main);
-
-//startQuiz
-//generate function-display question?
-
-//template is displaying
-
-
